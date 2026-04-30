30c993606e4f5f3da3a3ca3b79f4e98a1d44ce26
O-Guiih <guiihsilvasousa1@gmail.com>
2026-04-29 21:34:27 -0300

    Adicionando desafios 6, 7, 8, e 9 do Mundo 2

--- src/main/java/com/eniac/devevolution/config/SeederConfig.java
+++ src/main/java/com/eniac/devevolution/config/SeederConfig.java
@@ -65,8 +65,9 @@ public class SeederConfig implements CommandLineRunner {
             mundoRepository.save(mundo1);
             mundoRepository.save(mundo2);
 
-            // 3. Cadastrando as 5 Lições Oficiais!
+            // 3. Cadastrando as Lições Oficiais!
 
+            // Lições Mundo 1
             Desafio desafio1 = new Desafio();
             desafio1.setTitulo("Hello World");
             desafio1.setDescricaoMarkdown("Use `console.log()` para imprimir a mensagem na tela.");
@@ -103,8 +104,41 @@ public class SeederConfig implements CommandLineRunner {
             desafio5.setXpRecompensa(300);
             desafio5.setMundo(mundo1); // Atrelado ao Mundo 1 conforme solicitado
 
+            // Lições Mundo 2
+
+            Desafio desafio6 = new Desafio();
+            desafio6.setTitulo("Par ou Ímpar");
+            desafio6.setDescricaoMarkdown("O Conceito: Imagine que você tem um punhado de doces e quer dividi-los igualmente com um amigo.");
+            desafio6.setCodigoInicial("// Vamos descobrir se 7 doces é par ou ímpar!\nlet doces = 7;\n// 1. Crie a variável 'resto' usando doces % 2\nlet resto = \n// 2. Mostre o 'resto' no console para descobrir!\n console.log(resto);");
+            desafio6.setXpRecompensa(50);
+            desafio6.setMundo(mundo2);
+
+            Desafio desafio7 = new Desafio();
+            desafio7.setTitulo("O Caminho do if/Else ");
+            desafio7.setDescricaoMarkdown("O Conceito: Imagine que está a conduzir e chega a um cruzamento. A regra é simples: SE (if) o semáforo estiver verde, avança.SENÃO (else), tem de parar.");
+            desafio7.setCodigoInicial("// O semáforo pode estar \"verde\" ou \"vermelho\" \n if (semaforo === \"verde\") { \n // O que fazer se estiver verde? \n } else { \n // O que fazer se não estiver verde? \n };");
+            desafio7.setXpRecompensa(100);
+            desafio7.setMundo(mundo2);
+
+            Desafio desafio8 = new Desafio();
+            desafio8.setTitulo("Radar de Ranks: If / Else If / Else");
+            desafio8.setDescricaoMarkdown("Na guilda dos heróis, precisamos classificar os guerreiros com base na sua experiência (XP). Para isso, o if / else não é suficiente, pois temos 3 opções!");
+            desafio8.setCodigoInicial("// O Radar de Ranks detectou um guerreiro!\n let xp = 2500;\n let rank = \"\";\n// 1. Crie o caminho para \"Novato\" (xp menor que 1000)\n if (xp < 1000) {\n rank = \"Novato\";\n}\n// 2. Crie o caminho para \"Explorador\" (xp até 5000) usando else if\n else if (xp <= 5000) {\n rank = \"Explorador\";\n}\n// 3. E para quem tem mais de 5000? Use o else para \"Mestre\" \n else {\n \n}\n// O Radar anuncia o resultado\n console.log(\"\uD83C\uDF1F Atenção! Um guerreiro rank \" + rank + \" se aproxima!\");");
+            desafio8.setXpRecompensa(150);
+            desafio8.setMundo(mundo2);
+
+            Desafio desafio9 = new Desafio();
+            desafio9.setTitulo("A Porta dos Guardiões: E / OU Lógico");
+            desafio9.setDescricaoMarkdown("A Profecia: A Porta de Pedra está trancada há séculos. A lenda diz que ela só se abrirá para:Um aventureiro com o rank de \"Mestre\" ou Alguém que tenha a Chave de Cristal E Energia > 50");
+            desafio9.setCodigoInicial("// O aventureiro diante da porta:\n let rank = \"Explorador\";\n let chaveDeCristal = true;\n let energia = 60;\n // Regra: A porta só abre se (O Rank for 'Mestre') \n // OU (Tiver a Chave E a energia for maior que 50) \n // Complete as condições dentro do if! \n if ( rank === \"Mestre\" || (chaveDeCristal === true && energia > 50) ) { \n console.log(\"✨ A Porta de Pedra ruge e se abre! Bem-vindo, herói!\"); \n } else { \n console.log(\"❌ A porta permanece selada. Volte quando for digno!\");}");
+            desafio9.setXpRecompensa(200);
+            desafio9.setMundo(mundo2);
+
+
+
+
             // A ordem aqui dita os IDs gerados pelo banco (1, 2, 3, 4, 5)
-            desafioRepository.saveAll(Arrays.asList(desafio1, desafio2, desafio3, desafio4, desafio5));
+            desafioRepository.saveAll(Arrays.asList(desafio1, desafio2, desafio3, desafio4, desafio5, desafio6, desafio7, desafio8, desafio9));
 
             System.out.println("✅ Mundos criados!");
         }
